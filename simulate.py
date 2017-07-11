1b2651c163b54b9608a77bbb73735abe73aa0cc9
Wenwei Lin <youdontseemehaha@gmail.com>
2017-07-11 01:34:27 -0700

    add senario 1 and 2 for simulating.

--- simulate.py
+++ simulate.py
@@ -10,10 +10,37 @@ Main Simulate Entry File
 """
 
 
-if __name__ == '__main__':
-    # office.write_letters()
-    # office.close_save_office()
-    browser = Browse()
+def senario_1():
+    """
+    - start office first and write something and save as docx file.
+    - Open browser and read inbox, compose email.
+    :return: 
+    """
+    office.start()
+    office.write_letters()
+    office.close_save_office()
     browser.start()
     browser.login()
     browser.read_inbox()
+    browser.compose_mail()
+    browser.logout()
+
+
+def senario_2():
+    """
+    Start browser first and login with username and password, read trash, inbox, and compose email
+    :return: 
+    """
+    browser.start()
+    browser.login()
+    browser.read_archive()
+    browser.read_trash()
+    browser.compose_mail()
+    browser.logout()
+    office.start()
+    office.write_letters()
+    office.close_save_office()
+
+if __name__ == '__main__':
+    senario_list = [senario_1, senario_2]
+    random.choice(senario_list)()
