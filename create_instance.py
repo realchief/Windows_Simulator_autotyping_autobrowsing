e9a4bbb00405d61c1e035f22e22f265e3b32789f
Wenwei Lin <youdontseemehaha@gmail.com>
2017-08-13 15:37:09 -0700

    add new first linux ami id(ubuntu)

--- create_instance.py
+++ create_instance.py
@@ -31,9 +31,10 @@ ec2_client = boto3.client('ec2',
                           region_name=data["AWS_REGION"])
 
 Instances = []
-Windows_Initial_Id = "ami-d4dfd1c2"
+Windows_Initial_Id = "ami-7da4ab6b"
 Windows_Instance_Id = "ami-2f250c4f"
 Linux_Instance_Id = "ami-9ec9e1fe"
+Linux_Initial_Id = "ami-cd0f5cb6"
 
 Instance_Type_List = ['t2.nano', 't2.micro', 't2.small', 't2.medium', 't2.large', 't2.xlarge', 't2.2xlarge']
 
@@ -66,15 +67,15 @@ class Instance():
         Create the EC2 key file
         :return: 
         """
-        if os.path.isfile('KEY_FILES/Windowskey.pem'):
+        if os.path.isfile('KEY_FILES/SimulatorNew.pem'):
             return
 
-        with open('KEY_FILES/Windowskey.pem', 'w') as keyfile:
-            key_pair = ec2_resource.create_key_pair(KeyName='Windowskey')
+        with open('KEY_FILES/SimulatorNew.pem', 'w') as keyfile:
+            key_pair = ec2_resource.create_key_pair(KeyName='SimulatorNew')
             key_pair_out = str(key_pair.key_material)
             keyfile.write(key_pair_out)
 
-    def decrypt(self, ciphertext, keyfile=os.path.expanduser('KEY_FILES/Windowskey.pem')):
+    def decrypt(self, ciphertext, keyfile=os.path.expanduser('KEY_FILES/SimulatorNew.pem')):
 
         input = open(keyfile)
         key = RSA.importKey(input.read())
@@ -91,9 +92,9 @@ class Instance():
         response = ec2_client.describe_instances()
         # Get all the instances and search for the instance based on the provided Tag - Name
         for reservation in response["Reservations"]:
+
             for item in reservation["Instances"]:
                 try:
-                    print('Instance {}'.format(item))
                     password_data = ec2_client.get_password_data(InstanceId=item["InstanceId"])
                     print('Password Data: {}'.format(password_data))
                     self.win_pwd = self.decrypt(password_data['PasswordData'].decode('base64'))
@@ -103,21 +104,34 @@ class Instance():
                                                 'public_ip': item['PublicIpAddress']}))
                 except Exception as e:
                     print('Create Instance => Got Error: {}'.format(e))
-                    json_data.append(json.dumps({'id': item["InstanceId"],
-                                                 'type': item['InstanceType'],
-                                                 'winpwd': self.win_pwd,
-                                                 'public_ip': item['PublicIpAddress']}))
+                    json_data = []
+                    # json_data.append(json.dumps({'id': item["InstanceId"],
+                    #                              'type': item['InstanceType'],
+                    #                              'winpwd': self.win_pwd,
+                    #                              'public_ip': item['PublicIpAddress']}))
+        if len(json_data) != 0:
+            print "Writing json_data"
+            self.write_info(json_data)
 
-        self.write_info(json_data)
-
-    def create_multi_instances(self, Image_Id=Windows_Instance_Id, MinCount=1, MaxCount=1,
-                               Key_Name="Windowskey", SubnetId='subnet-6acf8d32', **kwargs):
+    def create_multi_instances(self, Image_flag=True, Window_Image=True, Linux_Image=False,
+                               MinCount=1, MaxCount=1, Key_Name="SimulatorNew",
+                               SubnetId='subnet-838ef8d9', **kwargs):
         """
         Create multi instances with Testkey file.
         :param num: 
         :return: 
         """
         global Instances
+
+        if Image_flag and Window_Image:
+            Image_Id = Windows_Initial_Id
+        elif Image_flag and Linux_Image:
+            Image_Id = Linux_Initial_Id
+        elif Window_Image:
+            Image_Id = Windows_Instance_Id
+        else:
+            Image_Id = Linux_Instance_Id
+
         try:
             for i in range(0, MaxCount):
                 # instance_type = random.choice(Instance_Type_List)
@@ -128,7 +142,6 @@ class Instance():
                     MinCount=1,
                     MaxCount=1,
                     KeyName=Key_Name,
-                    SubnetId=SubnetId
                 )
 
         except Exception as e:
@@ -161,14 +174,23 @@ def start():
     ap.add_argument("-l", "--linux", help="Create Linux Instance", action="store_true")
     ap.add_argument("-f", "--first", help="when use to make first instance", action="store_true")
     ap.add_argument("-n", "--number", default='1', help="The number of instances you want to make")
+    ap.add_argument("-c", "--credentials", help="credentials for remote access to window instance.",
+                    action="store_true")
+    ap.add_argument("-t", "--terminate", help="credentials for remote access to window instance.",
+                    action="store_true")
     args = vars(ap.parse_args())
     print('args: {}'.format(args))
-
     instance = Instance()
-    instance.create_key()
-    instance.create_multi_instances(MaxCount=1)
-    # instance.decrypt_ec2_secure_info()
-    # instance.terminate_multi_instances()
+
+    if args["credentials"]:
+        instance.decrypt_ec2_secure_info()
+
+    elif args["terminate"]:
+        instance.terminate_multi_instances()
+    else:
+        instance.create_key()
+        instance.create_multi_instances(Image_flag=args["first"], Window_Image=args["window"], Linux_Image=args["linux"], MaxCount=int(args['number']))
+
 
 if __name__ == '__main__':
     start()
