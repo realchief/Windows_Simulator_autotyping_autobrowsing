513914f6213e8a37bff425f75454df9c4b5595a9
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-01-31 11:02:29 +0100

    final commit

--- automate_lib/web.py
+++ automate_lib/web.py
@@ -1,18 +1,34 @@
 import webbrowser
 from random import choice
-import random
+# import randomVancouver
 import newspaper
 from newspaper import Article
 from keyboard import *
 
+# news_urls = [
+#     'http://www.indiatimes.com',
+#     'http://www.bild.de/',
+#     'http://www.thesun.co.uk/',
+#     'https://www.nytimes.com/',
+#     'http://cnn.com',
+#     # 'http://sina.com.cn'
+# ]
+
 news_urls = [
-    'http://www.indiatimes.com',
-    'http://www.bild.de/',
-    'http://www.thesun.co.uk/',
-    'https://www.nytimes.com/',
-    'http://cnn.com',
-    # 'http://sina.com.cn'
+    {'url': 'http://www.indiatimes.com', 'lg': 'en'},
+    {'url': 'http://www.bild.de/', 'lg': 'de'},
+    {'url': 'http://www.thesun.co.uk/', 'lg': 'en'},
+    {'url': 'https://www.nytimes.com/', 'lg': 'en'},
+    {'url': 'http://cnn.com', 'lg': 'en'},
+    {'url': 'http://www.xinhuanet.com', 'lg': 'zh'},
+    # {'url': 'http://www.pressonline.rs', 'lg': 'rs'},
+    # {'url': 'http://www.alo.rs', 'lg': 'rs'},
+    # {'url': 'http://www.kurir.rs', 'lg': 'rs'},
+    # {'url': 'http://www.novosti.rs', 'lg': 'rs'},
+    # {'url': 'https://www.blic.rs', 'lg': 'rs'},
+    {'url': 'http://www.aguilardigital.es/', 'lg': 'es'},
 ]
+
 
 # news_lang = ['zh', 'en', 'de']
 
@@ -21,13 +37,15 @@ def scrapy_content_newsurl():
     article_urls = []
     news_url = choice(news_urls)
     # language = choice(news_lang)
-    
+
     # if news_url == news_urls[5]:
     #     language = 'zh'
-    print('news url: {}'.format(news_url))
-    # print('language: {}'.format(language))
-    
-    news_paper = newspaper.build(news_url)
+    # print('news url: {}'.format(news_url))
+    # # print('language: {}'.format(language))
+    #
+    # news_paper = newspaper.build(news_url)
+    print('news url: {}'.format(news_url['url']))
+    news_paper = newspaper.build(news_url['url'])
 
     try:
         for article in news_paper.articles:
@@ -40,7 +58,7 @@ def scrapy_content_newsurl():
                 scrapy_content_newsurl()
 
             else:
-                url = article_urls[random.randint(0, len(article_urls)-1)]
+                url = article_urls[random.randint(0, len(article_urls) - 1)]
                 a = Article(url)
                 a.download()
                 a.parse()
@@ -64,4 +82,4 @@ def scrapy_content_newsurl():
 
 if __name__ == '__main__':
     content = scrapy_content_newsurl()
-    print ('content: {}'.format(content))
+    print('content: {}'.format(content))
